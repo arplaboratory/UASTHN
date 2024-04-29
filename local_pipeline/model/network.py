99aa93bf098003915fc7d1712954220f603e52d8
Jiuhong Xiao <jx1190@log-2.hpc.nyu.edu>
2024-04-29 15:52:08 -0400

    maj vote

--- local_pipeline/model/network.py
+++ local_pipeline/model/network.py
@@ -335,7 +335,7 @@ class UAGL():
             self.image_2 = tgm.crop_and_resize(self.image_2, bbox_s, (self.args.resize_width, self.args.resize_width))
         elif self.args.ue_aug_method == "mask":
             self.image_2 = self.image_2.view(B, self.args.ue_num_crops, C, H, W)
-            mask = torch.from_numpy(self.ue_rng.random((self.image_2.shape[0], int(self.args.ue_num_crops - 1), 1, self.image_2.shape[3]//self.args.ue_mask_patchsize, self.image_2.shape[4]//self.args.ue_mask_patchsize))).to(self.image_2.device) > self.args.ue_mask_prob
+            mask = torch.rand((self.image_2.shape[0], int(self.args.ue_num_crops - 1), 1, self.image_2.shape[3]//self.args.ue_mask_patchsize, self.image_2.shape[4]//self.args.ue_mask_patchsize)).to(self.image_2.device) > self.args.ue_mask_prob
             mask = torch.repeat_interleave(torch.repeat_interleave(mask, self.args.ue_mask_patchsize, dim=3), self.args.ue_mask_patchsize, dim=4)
             self.image_2[:, 1:] = self.image_2[:, 1:] * mask
             self.image_2 = self.image_2.view(B*self.args.ue_num_crops, C, H, W)            
@@ -416,6 +416,14 @@ class UAGL():
                 count = 1
                 for j in range(1,self.args.ue_num_crops):
                     if torch.norm(four_pred_five_crops[i, 0] - four_pred_five_crops[i, j]) <= resize_maj_vote_rej:
+                        four_pred_agg+=four_pred_five_crops[i, j]
+                        count+=1
+                four_pred_agg/=count
+            elif self.args.ue_agg == "maj_vote_mean":
+                four_pred_agg = four_pred_five_crops[i, 0].clone()
+                count = 1
+                for j in range(1,self.args.ue_num_crops):
+                    if torch.norm(mean_four_pred_five_crops[i] - four_pred_five_crops[i, j]) <= resize_maj_vote_rej:
                         four_pred_agg+=four_pred_five_crops[i, j]
                         count+=1
                 four_pred_agg/=count
